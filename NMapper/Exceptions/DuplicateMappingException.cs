03159bbcc96c357e80fb60f9c40cf2c2c89089eb
thomasgalliker <thomas@bluewin.ch>
2026-01-02 13:13:56 +0100

    Improve exception message text + add equality

--- NMapper/Exceptions/DuplicateMappingException.cs
+++ NMapper/Exceptions/DuplicateMappingException.cs
@@ -1,16 +1,74 @@
-﻿namespace NMapper
+﻿using NMapper.Extensions;
+
+namespace NMapper
 {
-    public class DuplicateMappingException : Exception
+    public class DuplicateMappingException : Exception, IEquatable<DuplicateMappingException>
     {
         public DuplicateMappingException(Type sourceType, Type targetType)
-            : base($"Duplicate mapping registered for {sourceType.Name} → {targetType.Name}")
+            : base(GetMessage(sourceType, targetType))
         {
             this.SourceType = sourceType;
             this.TargetType = targetType;
         }
 
+        private static string GetMessage(Type sourceType, Type targetType)
+        {
+            var sourceTypeName = sourceType.GetFormattedName();
+            var targetTypeName = targetType.GetFormattedName();
+
+            if (sourceTypeName == targetTypeName)
+            {
+                sourceTypeName = sourceType.GetFormattedFullname();
+                targetTypeName = targetType.GetFormattedFullname();
+            }
+
+            return $"Duplicate mapping registered for {sourceTypeName} → {targetTypeName}";
+        }
+
         public Type SourceType { get; }
 
         public Type TargetType { get; }
+
+        public bool Equals(DuplicateMappingException? other)
+        {
+            if (ReferenceEquals(other, null))
+            {
+                return false;
+            }
+
+            if (ReferenceEquals(this, other))
+            {
+                return true;
+            }
+
+            return this.SourceType == other.SourceType
+                && this.TargetType == other.TargetType;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return this.Equals(obj as DuplicateMappingException);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                int hash = 17;
+                hash = (hash * 23) + (this.SourceType != null ? this.SourceType.GetHashCode() : 0);
+                hash = (hash * 23) + (this.TargetType != null ? this.TargetType.GetHashCode() : 0);
+                return hash;
+            }
+        }
+
+        public static bool operator ==(DuplicateMappingException? left, DuplicateMappingException? right)
+        {
+            return Equals(left, right);
+        }
+
+        public static bool operator !=(DuplicateMappingException? left, DuplicateMappingException? right)
+        {
+            return !Equals(left, right);
+        }
     }
 }
--- NMapper/Exceptions/MappingException.cs
+++ NMapper/Exceptions/MappingException.cs
@@ -1,13 +1,30 @@
-﻿namespace NMapper
+﻿using NMapper.Extensions;
+
+namespace NMapper
 {
-    public class MappingException : Exception
+    public class MappingException : Exception, IEquatable<MappingException>
     {
         public MappingException(Type sourceType, Type targetType, Type mappingType, Exception ex)
-              : base($"Failed to map from {sourceType.Name} → {targetType.Name} using mapping {mappingType.Name}", ex)
+            : base(GetMessage(sourceType, targetType, mappingType), ex)
         {
             this.SourceType = sourceType;
             this.TargetType = targetType;
             this.MappingType = mappingType;
+        }
+
+        private static string GetMessage(Type sourceType, Type targetType, Type mappingType)
+        {
+            var sourceTypeName = sourceType.GetFormattedName();
+            var targetTypeName = targetType.GetFormattedName();
+            var mappingTypeName = mappingType.GetFormattedName();
+
+            if (sourceTypeName == targetTypeName)
+            {
+                sourceTypeName = sourceType.GetFormattedFullname();
+                targetTypeName = targetType.GetFormattedFullname();
+            }
+
+            return $"Failed to map from {sourceTypeName} → {targetTypeName} using mapping {mappingTypeName}";
         }
 
         public Type SourceType { get; }
@@ -15,5 +32,49 @@
         public Type TargetType { get; }
 
         public Type MappingType { get; }
+
+        public bool Equals(MappingException? other)
+        {
+            if (ReferenceEquals(other, null))
+            {
+                return false;
+            }
+
+            if (ReferenceEquals(this, other))
+            {
+                return true;
+            }
+
+            return this.SourceType == other.SourceType
+                && this.TargetType == other.TargetType
+                && this.MappingType == other.MappingType;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return this.Equals(obj as MappingException);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                int hash = 17;
+                hash = (hash * 23) + this.SourceType.GetHashCode();
+                hash = (hash * 23) + this.TargetType.GetHashCode();
+                hash = (hash * 23) + this.MappingType.GetHashCode();
+                return hash;
+            }
+        }
+
+        public static bool operator ==(MappingException? left, MappingException? right)
+        {
+            return Equals(left, right);
+        }
+
+        public static bool operator !=(MappingException? left, MappingException? right)
+        {
+            return !Equals(left, right);
+        }
     }
 }
