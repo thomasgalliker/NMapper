31a80beeb0d28bb766a76d2fa50419a353e502eb
thomasgalliker <thomas@bluewin.ch>
2026-01-02 16:28:12 +0100

    Extract interface to new file

--- NMapper/IMapper.cs
+++ NMapper/IMapper.cs
@@ -18,13 +18,4 @@ namespace NMapper
         [return: NotNullIfNotNull(nameof(source))]
         TTarget? Map<TSource, TTarget>(TSource? source);
     }
-
-    public interface IMappingContext
-    {
-        [return: NotNullIfNotNull(nameof(source))]
-        TTarget? Map<TTarget>(object? source);
-
-        [return: NotNullIfNotNull(nameof(source))]
-        TTarget? Map<TSource, TTarget>(TSource? source);
-    }
 }
