73a016f9c708904ca2c88f93cc86c796103ff45f
thomasgalliker <thomas@bluewin.ch>
2026-01-22 19:40:46 +0100

    Add mapping

---
+++ Tests/NMapper.TestData/Mappings/NullableDecimalToDoubleMapping.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace NMapper.TestData.Mappings
+{
+    public class NullableDecimalToDoubleMapping : IMapping<decimal?, double>
+    {
+        public double Map(decimal? source)
+        {
+            if (source == null)
+            {
+                return double.NaN;
+            }
+
+            return Convert.ToDouble(source);
+        }
+    }
+}
